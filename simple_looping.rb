f0e90c820e2a4981dcd8f66e1f6eb3f4c5b3ec74
Tanner Lancaster <tanner@flatironschool.com>
2018-05-29 21:35:17 +0000

    automatically backed up by learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -33,8 +33,8 @@ def while_iterator(number_of_times)
   phrase = "Welcome to Flatiron School's Web Development Course!"
   counter = 0
   while counter <= number_of_times
-    puts phrase
     counter += 1
+    puts phrase
   end
 
 end
